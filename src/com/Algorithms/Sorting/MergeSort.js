42bc60c97b29399513fd8300aa83f7a1ed0802fb
Kushagra Seth <kseth919@gmail.com>
2020-07-26 19:49:20 +0530

    Merge Sort Implementation

--- src/com/Algorithms/Sorting/MergeSort.js
+++ src/com/Algorithms/Sorting/MergeSort.js
@@ -1,12 +1,42 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-//TC -->
-//SC -->
+//TC --> O(n log n)
+//SC --> O(n)
+//Link: https://www.youtube.com/watch?v=mB5HXBb_HY8
 /*
-
+Uses divide and conquer technique, i.e., Recursion
 */
-const mergeSort = array => {
 
+const mergeSort = array => {
+    if (array.length === 1) {
+        return array;
+    }
+    // Split Array in into left and right
+    let length = array.length;
+    let middle = Math.floor(length/2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, length);
+
+    return merge(
+        mergeSort(left),
+        mergeSort(right)
+    );
+};
+
+const merge = (left, right) => {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 };
 
 console.log("Merge Sort: ", mergeSort(numbers));
