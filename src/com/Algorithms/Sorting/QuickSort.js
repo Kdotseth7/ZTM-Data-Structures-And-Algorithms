636e5e22f2e5c891505c297783efde7d22fdb78b
Kushagra Seth <kseth919@gmail.com>
2020-07-27 20:45:16 +0530

    Quick Sort Implementation

---
+++ src/com/Algorithms/Sorting/QuickSort.js
@@ -0,0 +1,44 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+//TC --> Best Case: O(n log n), Worst Case: O(n^2)
+//SC --> Best Case: O(log n), Worst Case: O(n) if there is a function call for every element,i.e., n function calls in call stack
+//Link: https://www.youtube.com/watch?v=7h1s2SojIRw, https://www.youtube.com/watch?v=-qOVVRIZzao
+/*
+Uses divide and conquer technique, i.e., Recursion
+*/
+
+const quickSort = (array, left, right) => {
+    debugger;
+    let pivot = left;
+    let partitionIndex;
+    if (left < right) {
+        partitionIndex = partition(array, pivot, left, right);
+        //Sort Left and Right
+        quickSort(array, left, partitionIndex);
+        quickSort(array, partitionIndex + 1, right);
+    }
+    return array;
+};
+
+const partition = (array, pivot, left, right) => {
+    while (left < right) {
+        while (array[pivot] >= array[left]) {
+            left++;
+        }
+        while (array[pivot] < array[right]) {
+            right--;
+        }
+        if (left < right)
+            swap(array, left, right);
+    }
+    swap(array, pivot, right);
+    return right;
+};
+
+const swap = (array, firstIndex, secondIndex) => {
+    let temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+console.log("Quick Sort: ", quickSort(numbers, 0, numbers.length - 1));
