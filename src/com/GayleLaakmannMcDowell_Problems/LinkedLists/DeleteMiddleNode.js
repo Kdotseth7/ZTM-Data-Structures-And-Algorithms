27e53a42f655ba7062ede32327d7a9274504bd34
Kushagra Seth <kseth919@gmail.com>
2020-09-12 22:17:26 +0530

    Delete MiddleNode

---
+++ src/com/GayleLaakmannMcDowell_Problems/LinkedLists/DeleteMiddleNode.js
@@ -0,0 +1,87 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this.print();
+    }
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this.print();
+    }
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+    insert(index, value) {
+        if (index === 0) return this.prepend(value);
+        if (index > this.length) return this.append(value);
+        const newNode = new Node(value);
+        const prevNode = this.traverseToIndex(index - 1);
+        const postNode = prevNode.next;
+        prevNode.next = newNode;
+        newNode.next = postNode;
+        this.length++;
+        return this.print();
+    }
+    remove(index) {
+        if (index > this.length)
+            return false;
+        const prevNode = this.traverseToIndex(index - 1);
+        const unwantedNode = prevNode.next;
+        prevNode.next = unwantedNode.next;
+        this.length--;
+        return unwantedNode;
+    }
+    deleteMiddleNode() {
+        const middleNodeIndex = Math.floor(this.length/2);
+        const prevNode = this.traverseToIndex(middleNodeIndex - 1);
+        const middleNode = prevNode.next;
+        prevNode.next = middleNode.next;
+        this.length--;
+        return this.print();
+    }
+    print() {
+        let printList = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            printList.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return printList;
+    }
+}
+
+const myLinkedList = new LinkedList(1);
+console.log(myLinkedList.append(5));
+console.log(myLinkedList.append(9));
+console.log(myLinkedList.prepend(2));
+console.log(myLinkedList.prepend(8));
+console.log(myLinkedList.insert(3, 6));
+console.log(myLinkedList.insert(98, 0));
+console.log(myLinkedList.remove(2));
+console.log(myLinkedList.print());
+console.log(myLinkedList.deleteMiddleNode());
+console.log(myLinkedList.deleteMiddleNode());
+
